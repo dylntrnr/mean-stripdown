5cb2e59d263207299cf8d2303092f20ec71c001e
Dylan Turner <dylan.turner22@gmail.com>
2013-09-03 09:05:51 -0600

    now I am at the 'views' section on the tutorial

--- public/js/config.js
+++ public/js/config.js
@@ -1,7 +1,17 @@
 //Setting up route
 window.app.config(['$routeProvider', function($routeProvider) {
 	$routeProvider
-	.when('/', { templateUrl: 'views/index.html' })
+	.when('/', {
+          templateUrl: 'views/index.html'
+        })
+        .when('/nflteams',
+          {
+            templateUrl: 'views/nfl/list.html'
+          })
+        .when('/nflteams/:nflTeamId',
+          {
+            templateUrl: 'views/nfl/views.html'
+        })
 	.otherwise({redirectTo: '/'});
 }]);
 
